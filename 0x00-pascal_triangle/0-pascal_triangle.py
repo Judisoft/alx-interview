451e5fbb978141101fab2a154a3ec6654e3ec0bf
Judisoft <kumjude09@gmail.com>
2023-06-30 00:40:20 +0000

    code restructured

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -7,14 +7,14 @@ def pascal_triangle(n):
     :n: an integer
 
     """
-    triangle = []
-    for row_idx in range(n):
-        current_row = [None] * (row_idx+1)
-        # first and last elements of current row are always 1
-        current_row[0], current_row[-1] = 1, 1
-        for col_idx in range(1, row_idx):
-            above_to_left_elt = triangle[row_idx - 1][col_idx-1]
-            above_to_right_elt = triangle[row_idx - 1][col_idx]
-            current_row[col_idx] = above_to_left_elt + above_to_right_elt
-        triangle.append(current_row)
-    return triangle
+    pascal_list = []
+    if n <= 0:
+        return pascal_list
+    for i in range(n):
+        row = [1]
+        if pascal_list:
+            last_row = pascal_list[-1]
+            row.extend([sum(pair) for pair in zip(last_row, last_row[1:])])
+            row.append(1)
+        pascal_list.append(row)
+    return (pascal_list)
